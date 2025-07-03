ea7b1661519b3e71c04244c038f96d01f36a38e2
Simon Swart <simonswart91@gmail.com>
2025-07-04 00:30:26 +0200

    Added slider and also display properties to the images

--- script.js
+++ script.js
@@ -1,89 +1,11 @@
-function hide0() {
-  var x = document.getElementById("s0");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide1() {
-  var x = document.getElementById("s1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide2() {
-  var x = document.getElementById("s2");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide3() {
-  var x = document.getElementById("s3");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide4() {
-  var x = document.getElementById("s4");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide5() {
-  var x = document.getElementById("s5");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide6() {
-  var x = document.getElementById("s6");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide7() {
-  var x = document.getElementById("s7");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide8() {
-  var x = document.getElementById("s8");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function hide9() {
-  var x = document.getElementById("s9");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+//slider controls
+function slider() {
+  const value = document.getElementById("slider").value;
 
-function hide10() {
-  var x = document.getElementById("s10");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+  for (let i = 0; i <= 10; i++) {
+    const img = document.getElementById("s" + i);
+    if (img) {
+      img.style.display = (i == value) ? "block" : "none";
+    }
   }
 }
